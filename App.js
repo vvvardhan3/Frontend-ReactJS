0e19871ae6c88014a1bf2a253a8ce89210b86ecc
Vishnu Vardhan Vankayalapati <vishnuvankayalapati@giggi.io>
2023-10-15 01:03:16 -0400

    update

--- App.js
+++ App.js
@@ -3,22 +3,26 @@
 // In React, Order of files matters alot!!
 // React Element is Object
 // ReactDom is responsible for assigning!!
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
     "div",
     { id: "parent" },
     [React.createElement("div",
         { id: "child" },
-        [React.createElement("h1", {}, "I'm H1 tag"),
-        React.createElement("h2", {}, "I'm H2 tag")]
+        [React.createElement("h1", {}, "I'm Sorry"),
+        React.createElement("h2", {}, "I'm Sorry")]
     ), React.createElement("div",
         { id: "child2" },
-        [React.createElement("h1", {}, "I'm H1 tag"),
-        React.createElement("h2", {}, "I'm H2 tag")]
+        [React.createElement("h1", {}, "I'm Sorry"),
+        React.createElement("h2", {}, "I'm Sorry")]
     )
-]);
+    ]);
 
 // const heading = React.createElement("h1", { id: "heading"}, "Hello World from React");
 console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); 
+root.render(parent);
 // Everything that is in the root we be replaced in the div after using .render function
---
+++ README.md
@@ -0,0 +1,21 @@
+# React
+
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR (Hot Module Replacement)
+- File Watching Algorithm - Written in C++
+- Faster Builds - Cashing things up
+- Image Optimization
+- Minification of files in production
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - In accordance with the different web browsers
+- Diagnostics
+- Error handling
+- Hosts on HTTPS
+- Tree Shaking - Will remove the unused code 
+- Different dev and prod bundles
