97dd06d15ddaa0fbd24dd8112ad532d83d3bc9cf
Vishnu Vardhan Vankayalapati <vishnuvankayalapati@giggi.io>
2023-10-16 21:54:54 -0400

    JSX

--- App.js
+++ App.js
@@ -23,20 +23,24 @@ import ReactDOM from "react-dom/client";
 //     )
 //     ]);
 
-// // const heading = React.createElement("h1", { id: "heading"}, "Hello World from React");
+// const heading = React.createElement("h1", { id: "heading"}, "Hello World from React");
 // console.log(parent);
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(parent);
-// // Everything that is in the root we be replaced in the div after using .render function
+// Everything that is in the root we be replaced in the div after using .render function
 
 // Core React coding, Which is not developer friendly!
-const heading = React.createElement("h1",{ id: "heading"},"Namaste, Madam!");
-
-console.log(heading);
+// React.createElement => ReactElement, Which is JS Object at end-of-day => HTMLElement, While Rendering!
+// const heading = React.createElement("h1",{ id: "heading"},"Namaste, Madam!");
 
 // JSX - HTML-like Syntax or XML-like Syntax
-// JSX is transpiled even before it reaches the JS engine by Parcel with the help of Babel
-const jsxheading = <h1 id="heading">Namaste, VishnuVardhan!</h1>;
+// JSX is transpiled even before it reaches the JS engine by Parcel with the help of Babel (JS Compiler)
+// JSX => React.createElement => ReactElement, Which is JS Object at end-of-day => HTMLElement, While Rendering!
+// How JSX is different from HTML; In HTML we write just "Class" but in JSX, We write it as "ClassName"
+// Attributes to JSX, We have to use "CamelCase"
+const jsxheading = (<h1 id="heading">
+    Namaste, VishnuVardhan!
+    </h1>); // We are using round-brakets because babel needs to understand this as a 
 
 console.log(jsxheading);
 
