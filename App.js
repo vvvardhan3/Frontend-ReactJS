9010b9a283fd89af2784821e692917bf560110b2
Vishnu Vardhan Vankayalapati <vishnuvankayalapati@giggi.io>
2023-10-16 21:08:44 -0400

    JSX Explaination

--- App.js
+++ App.js
@@ -7,22 +7,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    [React.createElement("div",
-        { id: "child" },
-        [React.createElement("h1", {}, "I'm Sorry"),
-        React.createElement("h2", {}, "I'm Sorry")]
-    ), React.createElement("div",
-        { id: "child2" },
-        [React.createElement("h1", {}, "I'm Sorry"),
-        React.createElement("h2", {}, "I'm Sorry")]
-    )
-    ]);
+// Class-1&2:
 
-// const heading = React.createElement("h1", { id: "heading"}, "Hello World from React");
-console.log(parent);
+// const parent = React.createElement(
+//     "div",
+//     { id: "parent" },
+//     [React.createElement("div",
+//         { id: "child" },
+//         [React.createElement("h1", {}, "I'm Sorry"),
+//         React.createElement("h2", {}, "I'm Sorry")]
+//     ), React.createElement("div",
+//         { id: "child2" },
+//         [React.createElement("h1", {}, "I'm Sorry"),
+//         React.createElement("h2", {}, "I'm Sorry")]
+//     )
+//     ]);
+
+// // const heading = React.createElement("h1", { id: "heading"}, "Hello World from React");
+// console.log(parent);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+// // Everything that is in the root we be replaced in the div after using .render function
+
+// Core React coding, Which is not developer friendly!
+const heading = React.createElement("h1",{ id: "heading"},"Namaste, Madam!");
+
+console.log(heading);
+
+// JSX - HTML-like Syntax or XML-like Syntax
+// JSX is transpiled even before it reaches the JS engine by Parcel with the help of Babel
+const jsxheading = <h1 id="heading">Namaste, VishnuVardhan!</h1>;
+
+console.log(jsxheading);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-// Everything that is in the root we be replaced in the div after using .render function
+
+root.render(jsxheading);
